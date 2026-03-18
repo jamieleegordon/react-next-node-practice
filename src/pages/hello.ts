aee8ebd81ac437a67e651cdd5fb5adfd9e5ac89d
Jamie-Lee Gordon <gordonjamie123@yahoo.co.uk>
2026-03-18 14:25:02 +0000

    init setup
    init setup

---
+++ src/pages/hello.ts
@@ -0,0 +1,9 @@
+// pages/api/hello.ts
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ message: 'Hello from Node.js API!' })
+}
